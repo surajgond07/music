4d46f8addbd03ca6f2fc9060fe498789c5b15eda
Suraj Gond <surajgond07@gmail.com>
2022-12-16 07:13:38 +0530

    Music player build with JavaScript.

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+//music 
+const songs = [{
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven National Army (Remix)',
+        artist: 'Jasinto Design',
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Goodnight Disco Queen',
+        artist: 'Jasinto Design',
+    },
+    {
+        name: 'metric-1',
+        displayName: 'Front Row (Remix)',
+        artist: 'Metrix/Jasinto Design',
+    },
+];
 
 // check it palying
 let isPlaying = false;
@@ -26,3 +52,41 @@ function pauseSong() {
 // Play or Pause Event Listner
 
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+//Update DOM
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+// Current Song
+let songIndex = 0;
+
+// Previous Song 
+function prevSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Next Song 
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// On Load - Select First Song
+loadSong(songs[songIndex]);
+
+// Event Listners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
